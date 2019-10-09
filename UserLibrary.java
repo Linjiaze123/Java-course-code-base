90f8171af285b9660c05f4bcfc291d4238b8181b
Linjiaze123 <1185137488@qq.com>
2019-10-09 20:17:27 +0800

    created a new class

---
+++ UserLibrary.java
@@ -0,0 +1,13 @@
+public class UserLibrary {
+	
+	boolean UserLibrary(String username,String password) {
+		String username2 = "1185137488";
+		String password2 = "qwe123";
+		
+		if(username2.equals(username)&&password2.equals(password)) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+}
