d185fb0cb9b075b56e87a783ef68bd94861b2555
Linjiaze123 <1185137488@qq.com>
2019-11-03 21:31:42 +0800

    created a new class

---
+++ ReadingMatterInte.java
@@ -0,0 +1,10 @@
+package com.classbook.chapter.six2;
+
+public interface ReadingMatterInte {
+    int price = 100;            
+    int pagination = 150;       
+
+    public void Effect();
+    public void FitObject();
+
+}
